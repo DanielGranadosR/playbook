232e47d275bfdaff304b0d4c0b018dbbc949a5b6
Dan Granados <anmedicto@gmail.com>
2022-04-25 18:46:34 -0500

    Test

---
+++ weekly_mission_3/my_launch_app/index.js
@@ -0,0 +1,7 @@
+const MissionCommander = require('./app/missionCommander');
+const objeto1 = new MissionCommander("X")
+console.log(objeto1.name)
+const objeto2 = new MissionCommander("Y")
+console.log(objeto2.name)
+const objeto3= new MissionCommander("Z")
+console.log(objeto3.name)
